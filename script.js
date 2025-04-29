50eb6cc7eed9b16c4176ddb173c66dfd4492e480
Fernanda Martins <141344853+nandinhafer@users.noreply.github.com>
2025-04-28 21:02:58 -0300

    teste menu novo

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-const cards = document.querySelectorAll('.card');
-
-cards.forEach(card => {
-    let currentImage = 0;
-    const images = card.querySelectorAll('.image');
-
-    card.addEventListener('mouseover', () => {
-        currentImage = (currentImage + 1) % images.length;
-        images.forEach((img, index) => {
-            img.classList.toggle('hidden', index !== currentImage);
-        });
-    });
-});
 
 
 function toggleMenu() {
